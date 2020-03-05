022e5a57bb43ad9ed0e629ea2754059b7839df7b
EvgeniyBudaev <49036840+EvgeniyBudaev@users.noreply.github.com>
2020-03-05 10:42:04 +0300

    89. Авторизация

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classes from "./Auth.module.css";
 import Button from "../../components/UI/Button/Button";
 import Input from "../../components/UI/Input/Input";
+import axios from "axios";
 // import is from "is_js";
 
 function validateEmail(email) {
@@ -40,9 +41,39 @@ export default class Auth extends Component {
     }
   };
 
-  loginHandler = () => {};
+  loginHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.value,
+      returnSecureToken: true
+    };
+    try {
+      const response = await axios.post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDjbHNYHQ5MC05LmmJrnt_xYEIcetlylmA",
+        authData
+      );
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
-  registerHandler = () => {};
+  registerHandler = async () => {
+    const authData = {
+      email: this.state.formControls.email.value,
+      password: this.state.formControls.value,
+      returnSecureToken: true
+    };
+    try {
+      const response = await axios.post(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDjbHNYHQ5MC05LmmJrnt_xYEIcetlylmA",
+        authData
+      );
+      console.log(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
 
   submitHandler = event => {
     event.preventDefault();
